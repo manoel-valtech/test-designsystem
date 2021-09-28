caaeaa2f1e6034a18043e2677789f5f877f2b99b
Manoel dos Santos <manoel.dossantos@valtech.com>
2021-09-28 15:43:13 -0400

    Fixed babel config

---
+++ dist/Hero/Hero.js
@@ -0,0 +1,10 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.Hero = Hero;
+
+function Hero() {
+  return /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h1", null, "Hellow World!"), /*#__PURE__*/React.createElement("p", null, "This is my test "));
+}
--- dist/Hero/Hero.tsx
+++
@@ -1,8 +0,0 @@
-export function Hero () {
-    return (
-        <div>
-            <h1>Hellow World!</h1>
-            <p>This is my test </p>
-        </div>
-    )
-}
